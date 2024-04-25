469f07e321eb926efa10b15319f2575f18e448c1
Luis Sanchez <luis@dooper.mx>
2024-04-24 23:22:19 -0500

    feat: Add tremor

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,16 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { SearchSelect, SearchSelectItem } from "@tremor/react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="mx-auto max-w-xs">
+      <div className="mb-4 mt-8 text-center font-mono text-sm text-slate-500">
+        Select city
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+      <SearchSelect>
+        <SearchSelectItem value="1">Option 1</SearchSelectItem>
+        <SearchSelectItem value="2">Option 2</SearchSelectItem>
+        <SearchSelectItem value="3">Option 3</SearchSelectItem>
+      </SearchSelect>
+    </div>
+  );
+};
